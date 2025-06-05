eb4f48c5dbb9feb843d3696a7686532d6c1a1bcd
alhusseinweb <154866823+alhusseinweb@users.noreply.github.com>
2025-06-05 18:39:16 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -146,20 +146,17 @@ Route::middleware(['auth', EnsureUserIsAdmin::class])
 
         Route::get('settings/sms', [SmsSettingController::class, 'edit'])->name('settings.sms.edit');
         Route::patch('settings/sms', [SmsSettingController::class, 'update'])->name('settings.sms.update');
-
-        // --- MODIFICATION START: Add Routes for Add-on Services ---
-Route::prefix('admin')->name('admin.')->group(function () {
-    Route::resource('add-on-services', \App\Http\Controllers\Admin\AddOnServiceController::class, [
-        'names' => [
-            'index' => 'add_on_services.index',
-            'create' => 'add_on_services.create',
-            'store' => 'add_on_services.store',
-            'edit' => 'add_on_services.edit',
-            'update' => 'add_on_services.update',
-            'destroy' => 'add_on_services.destroy',
-        ]
-    ])->except(['show']);
-});
+// --- MODIFICATION START: Add Routes for Add-on Services ---
+Route::resource('add-on-services', AddOnServiceController::class, [
+    'names' => [
+        'index' => 'add_on_services.index',
+        'create' => 'add_on_services.create',
+        'store' => 'add_on_services.store',
+        'edit' => 'add_on_services.edit',
+        'update' => 'add_on_services.update',
+        'destroy' => 'add_on_services.destroy',
+    ]
+])->except(['show']);
 // --- MODIFICATION END ---
          
         Route::post('data-management/delete-all-bookings', [DataManagementController::class, 'deleteAllBookingsAndRelatedData'])->name('data.delete_all_bookings');
