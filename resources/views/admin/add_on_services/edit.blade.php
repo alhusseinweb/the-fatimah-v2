baaea71410d8093427168121e42ee5944959a598
alhusseinweb <154866823+alhusseinweb@users.noreply.github.com>
2025-06-05 15:58:29 +0300

    Create edit.blade.php

---
+++ resources/views/admin/add_on_services/edit.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.admin')
+
+@section('title', 'تعديل الخدمة الإضافية: ' . $addOnService->name_ar)
+
+@section('content')
+    <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+        <h1 class="h2">تعديل الخدمة الإضافية: {{ $addOnService->name_ar }}</h1>
+    </div>
+
+    <div class="card shadow-sm border-0">
+        <div class="card-body">
+            <form action="{{ route('admin.add_on_services.update', $addOnService->id) }}" method="POST">
+                @method('PUT')
+                @include('admin.add_on_services._form', ['addOnService' => $addOnService])
+            </form>
+        </div>
+    </div>
+@endsection
